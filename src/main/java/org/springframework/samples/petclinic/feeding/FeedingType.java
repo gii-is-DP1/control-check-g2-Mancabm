75cc2f41b6183a7097446a79ddb3068fb126d6f5
Mancbm <silverlancernohr@gmail.com>
2022-01-14 10:17:04 +0100

    Lab Control Check

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,17 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +19,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "feeding_type", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class FeedingType {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @NotEmpty
+    @Size(min = 5, max = 30)
     String name;
+
+    @NotEmpty
     String description;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "pet_type_id")
     PetType petType;
 }
